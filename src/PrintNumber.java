66c6660665e1760c27a943ad7c6d49960f8abbb8
AykutCihan <aykutcihandemir@protonmail.com>
2023-03-09 17:16:57 +0100

    print numbers using without any numbers

---
+++ src/PrintNumber.java
@@ -0,0 +1,12 @@
+public class PrintNumber {
+
+//    Write a Java program to print numbers from 1 to 100 on the console without using any numbers.
+
+        public static void main(String[] args) {
+            for (int i = 'd' / 'd'; i <= 'd'; i++) {
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+
--- src/UniqueCharacterDetector/UsingIndexOfAndLastIndexOf.java
+++ src/UniqueCharacterDetector/UsingIndexOfAndLastIndexOf.java
@@ -1,4 +1,4 @@
-package Questions.UniqueCharacterDetector;
+package UniqueCharacterDetector;
 
 public class UsingIndexOfAndLastIndexOf {
 
